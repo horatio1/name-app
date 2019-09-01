b2534d0f7ec28e20ece330fa2cdb7853f3709b3a
Horatio Amariei <horatioamariei1@gmail.com>
2019-09-01 10:19:21 -0400

    feat: Added styling; fixed loading/display of data; added sentence grammar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,9 @@ export class AppComponent {
   public age: number;
   public gender: string;
   public nationalities: string[];
+  public loadedObj;
+  public loadedBool = false;
+  public missedArr: string = "";
 
   constructor (private http: HttpClient) { }
 
@@ -34,22 +37,74 @@ export class AppComponent {
   }
 
   guessInfoClick(nameInputValue) {
+    this.loadedBool = false;
+    this.loadedObj = this.emptyLoaded(); // Make empty
     this.displayName = nameInputValue;
     this.getAge(nameInputValue).subscribe((ageData: any) => {
       this.age = ageData.age;
+      this.loaded("age");
       // TODO: Extra stats (count)
     });
     this.getGender(nameInputValue).subscribe((genderData: any) => {
       this.gender = genderData.gender;
+      this.loaded("gender");
     });
     this.getNationality(nameInputValue).subscribe((nationalityData: any) => {
       this.nationalities = [];
       nationalityData.country.forEach((country: any, index: number) => {
         this.nationalities.push('');
+        this.loaded("nationalities");
         this.getCountryByCode(country.country_id).subscribe((countryData: any) => {
           this.nationalities[index] = countryData.demonym;
+          this.loaded("countryByCode");
         });
       });
+      if(!this.nationalities.length){
+        this.loaded("nationalities");
+        this.loaded("countryByCode");
+      }
     });
+  }
+
+  emptyLoaded(){
+    return {
+      age: false,
+      gender: false,
+      nationalities: false,
+      countryByCode: false
+    }
+  }
+
+  loaded(name){
+    this.loadedObj[name] = true;
+    this.checkAllLoaded();
+  }
+
+  checkAllLoaded(){
+    this.loadedBool = true;
+    for(const o in this.loadedObj) {
+      if(! this.loadedObj[o]) {
+        this.loadedBool = false;
+      }
+    }
+    if(this.loadedBool){
+      this.missedArr = this.arrayToSentence();
+    }
+    console.log(this.loadedObj);
+  }
+
+  arrayToSentence () {
+    let arr = [];
+    if(!this.age) {
+      arr.push("age");
+    }
+    if(!this.gender) {
+      arr.push("gender");
+    }
+    if(!this.nationalities || !this.nationalities.length) {
+      arr.push("nationality");
+    }
+    var last = arr.pop();
+    return arr.join(', ') + ((arr.length > 1) ? ' and ' : ' ') + last;
   }
 }
